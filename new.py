d5d3d71942c17d0ab291879d1effa2d0ec97a316
Gurjitsingh123 <112458981+Gurjitsingh123@users.noreply.github.com>
2022-08-30 16:27:04 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,20 @@
 from sqlite3 import connect
 import mysql.connector
-
+import pandas as pd
+li=[]
+li1=[]
 mydb = mysql.connector.connect(host="localhost",user="root",password="",database="training")
 mycursor=mydb.cursor()
-mycursor.execute("select * from products")
+mycursor.execute("select uXS,uS,uM,uL,uXL,uXXL from products")
 for i in mycursor:
-    print(i)
+   li.append(i)
+mycursor1=mydb.cursor()
+mycursor1.execute("select XS,S,M,L,XL,XXL from products")
+for j in mycursor1:
+    li1.append(j)
+dict={
+    'size':li,
+    'qty':li1
+}
+data=pd.DataFrame(dict)
+print(data)
